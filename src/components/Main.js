90af551b5d443abc90b4961cf7f1810c9fd5df64
Omkar <bhosaleomkar9606@gmail.com>
2022-10-11 19:37:27 +0530

    mobile view

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ function Main(){
             <small>Extensive knowledge as a system architech and software developer. Great with Java and Javascript
                 and ability to work in a team to make best of any situation.</small><br /><br />
             <h3>Interests</h3>
-            <small>Cricket and football are two of my favourite sports. Coiding has become one of my favourite hobby since last 
+            <small>Cricket and football are two of my favourite sports. Coding has become one of my favourite hobby since last 
                 few years.
             </small>
         </div>
